11c5db94452dd1b536bb38242844922d71d88b9b
Eric Hodges <ehodges2004@gmail.com>
2017-12-10 14:52:42 -0800

    added api endpoint to index.js

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -10,7 +10,7 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 // 2
-const httpLink = new HttpLink({ uri: '__SIMPLE_API_ENDPOINT__' })
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjax3rfxo0prm01426vyoyplj' })
 
 // 3
 const client = new ApolloClient({
